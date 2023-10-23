bb457daaa4f2d9fd391eaad2febadc18572dede4
Andres Ospina <andresospina0000@gmail.com>
2023-10-23 13:53:26 -0500

    Refactor

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -3,7 +3,7 @@ def call(boolean abortPipeline = false) {
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
     }
-    qg = waitForQualityGate
+    qg = waitForQualityGate()
     echo "Quality Gate: ${qg.status}"
     // timeout(time: 1, unit: 'MINUTES'){
     //     def qg = waitForQualityGate abortPipeline: abort
