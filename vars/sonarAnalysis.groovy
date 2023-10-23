868ff2efb05016e134b5f04bd42ecb258d72cb76
Andres Ospina <andresospina0000@gmail.com>
2023-10-22 20:42:14 -0500

    Se agrega funcion de timeout.

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,4 +2,7 @@ def call() {
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
     }
+    timeout(time: 5, unit: 'MINUTES') {
+        waitForQualityGate abortPipeline: true
+    }
 }
