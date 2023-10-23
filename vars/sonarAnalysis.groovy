ce4c721118da3e5c46e11db33630db71dbd026c7
Andres Ospina <andresospina0000@gmail.com>
2023-10-22 20:51:32 -0500

    Se agrega flag para abortar el pipeline.

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,8 +1,8 @@
-def call() {
+def call(boolean abortarPipeline = false) {
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
     }
     timeout(time: 5, unit: 'MINUTES') {
-        waitForQualityGate abortPipeline: true
+        waitForQualityGate abortPipeline: ${abortarPipeline}
     }
 }
