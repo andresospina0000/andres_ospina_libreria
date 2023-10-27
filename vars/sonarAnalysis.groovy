7b3fd899ac2a9f9ca398d55c44745aac21b895dd
Andres Ospina <andresospina0000@gmail.com>
2023-10-26 22:50:24 -0500

    Refactor del ejercicio 3 (incluye implementacion del opcional).

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,20 +1,12 @@
-def call(boolean abortPipeline, String gitBranch) {
+def call(boolean abortPipeline) {
     
-    echo "${abortPipeline} - ${gitBranch}"
+    echo "${abortPipeline}"
 
     if(abortPipeline){
         currentBuild.result = 'ABORTED'
         error('Ejecución abortada por el usuario!')
     }
-    def masterBranch = gitBranch.equalsIgnoreCase("master")
-    def hotfixBranch = gitBranch.startsWith("hotfix")
-
-    echo "${masterBranch} - ${hotfixBranch}"
-
-    if(masterBranch || hotfixBranch){        
-        currentBuild.result = 'ABORTED'        
-        error('Ejecución abortada. Rama master o hotfix!')
-    }
+    
     echo "SonarQube analysis"
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
