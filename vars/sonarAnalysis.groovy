e77234613a801bf31a7d7926043dbe111016b4b9
Andres Ospina <andresospina0000@gmail.com>
2023-10-26 21:52:22 -0500

    Test

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(boolean abortPipeline = false) {
     def abort = abortPipeline
-    echo ${abort}
+    echo abort
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
     }
