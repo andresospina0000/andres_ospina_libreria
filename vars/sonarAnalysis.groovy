f1adf37ccc7a860882b0c16b76349079583ca76e
Andres Ospina <andresospina0000@gmail.com>
2023-10-26 22:28:42 -0500

    Test.

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -13,7 +13,7 @@ def call(boolean abortPipeline, String gitBranch) {
     if(gitBranch.equalsIgnoreCase("master") || gitBranch.startsWith("hotfix")){
         abortPipeline: true
     }
-
+    echo "SonarQube analysis"
     withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'AO_Token') {
         sh "${tool("SonarScanner")}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver"
     }    
